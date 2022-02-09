f6f89cb282efc373f0115ac56689fea00a6bd05a
Volha Khainouskaya <volha.khainouskaya@gmail.com>
2022-02-10 00:57:26 +0300

    OnePageScroll (mobile touchSwipe)

--- scripts/ops.js
+++ scripts/ops.js
@@ -3,6 +3,9 @@ const display = $(".maincontent");
 const sideMenu = $(".fixed-menu");
 const menuItems = sideMenu.find(".fixed-menu__item");
 
+const mobileDetect = new MobileDetect(window.navigator.userAgent);
+const isMobile = mobileDetect.mobile();
+
 let inScroll = false;
 
 sections.first().addClass("active");
@@ -94,6 +97,8 @@ $(window).on("keydown", e => {
   }
 });
 
+$(".wrapper").on("touchmove", e => e.preventDefault());
+
 $("[data-scroll-to]").click(e => {
   e.preventDefault();
 
@@ -103,3 +108,17 @@ $("[data-scroll-to]").click(e => {
 
   performTransition(reqSection.index());
 });
+
+if (isMobile) {
+  $("body").swipe({
+  swipe: function (event, direction) {
+    const scroller = viewportScroller();
+    let scrollDirection = "";
+
+    if (direction === "up") scrollDirection = "next";
+    if (direction === "down") scrollDirection = "prev";
+
+    scroller[scrollDirection]();
+  }
+})
+}
